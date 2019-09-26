1e8596a4aa7645761c72cf8a7a527c510da41a97
kamquatz <kamquatz@users.noreply.github.com>
2019-09-26 09:20:23 +0300

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+   // Get the modal
+   var modal = document.getElementById('id01');
+                  
+   // When the user clicks anywhere outside of the modal, close it
+   window.onclick = function(event) {
+     if (event.target == modal) {
+       modal.style.display = "none";
+     }
+   };
+  
+  //  login scripts
+  
+  // Get the modal
+  var modal = document.getElementById('id02');
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  };
+  
